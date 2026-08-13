fa589f788dfe2ea199092cd2f9c4b2b455d5fa43
Chuck Newman, Jr. <33396894+chucknewmanjr@users.noreply.github.com>
2026-08-13 16:55:29 -0400

    Update Quick-XML-examples.sql

--- XML/Quick-XML-examples.sql
+++ XML/Quick-XML-examples.sql
@@ -3,21 +3,34 @@
 DECLARE @Vector XML = '<vectors><vector><x>2</x><y>3</y></vector><vector><x>5</x><y>7</y></vector></vectors>'
 
 SELECT @Vector.query('.')
+-- RETURNS: <vectors><vector><x>2</x><y>3</y></vector><vector><x>5</x><y>7</y></vector></vectors>
 
 SELECT c.query('.') FROM @Vector.nodes('.') t (c) -- same
+-- RETURNS: <vectors><vector><x>2</x><y>3</y></vector><vector><x>5</x><y>7</y></vector></vectors>
 
 SELECT c.query('.') FROM @Vector.nodes('*') t (c) -- same
+-- RETURNS: <vectors><vector><x>2</x><y>3</y></vector><vector><x>5</x><y>7</y></vector></vectors>
 
 SELECT c.query('.') FROM @Vector.nodes('*/*') t (c) -- star means all at the next level
+-- RETURNS: <vector><x>2</x><y>3</y></vector>
+--          <vector><x>5</x><y>7</y></vector>
 
 SELECT c.query('..') FROM @Vector.nodes('*/*') t (c) -- dot dot means go back a level
+-- RETURNS: <vectors><vector><x>2</x><y>3</y></vector><vector><x>5</x><y>7</y></vector></vectors>
+--          <vectors><vector><x>2</x><y>3</y></vector><vector><x>5</x><y>7</y></vector></vectors>
 
 SELECT c.query('*') FROM @Vector.nodes('*/*') t (c) 
+-- RETURNS: <x>2</x><y>3</y>
+--          <x>5</x><y>7</y>
 
 SELECT c.query('*') FROM @Vector.nodes('vectors/vector') t (c) 
+-- RETURNS: <x>2</x><y>3</y>
+--          <x>5</x><y>7</y>
 
 SELECT c.value('x[1]', 'int'), c.value('y[1]', 'int') FROM @Vector.nodes('vectors/vector') t (c) -- requires a singleton
-
+-- RETURNS:	2	3
+--        	5	7
+go
 
 -- ============================================================================
 -- parse a comma separated list
@@ -26,6 +39,10 @@ DECLARE @list VARCHAR(MAX) = 'Olivia, Emma, Charlotte'
 DECLARE @xml XML = '<x>' + REPLACE(@list, ',', '</x><x>') + '</x>'
 
 SELECT LTRIM(c.value('.', 'varchar(max)')) FROM @xml.nodes('x') t (c)
+--Olivia
+--Emma
+--Charlotte
+go
 
 -- ============================================================================
 -- parse a comma separated list
@@ -34,18 +51,28 @@ DECLARE @list VARCHAR(MAX) = 'Olivia, Emma, Charlotte'
 SELECT LTRIM(t2.c.value('.', 'varchar(max)')) 
 FROM (VALUES (CAST('<x>' + REPLACE(@list, ',', '</x><x>') + '</x>' AS XML))) t1 (c)
 CROSS APPLY t1.c.nodes('x') t2 (c)
+--Olivia
+--Emma
+--Charlotte
+go
 
 -- ============================================================================
 -- get tag name
 declare @xml xml = '<x><a>1</a><b>2</b><c>3</c></x>'
 
 select c.value('fn:local-name(.)', 'char(1)') from @xml.nodes('x/*') t (c)
+--a
+--b
+--c
+go
 
 -- ============================================================================
 -- get attribute value
 declare @xml xml = '<x><a type="11">1</a><a type="22">2</a><a type="33">3</a></x>'
 
 select c.value('@type', 'int') from @xml.nodes('x/a') t (c)
-
-
+--11
+--22
+--33
+go
 
